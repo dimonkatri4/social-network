27f86aa51d8e0195c59c16416de988b1a364e89f
Dima <katri4dimon@i.ua>
2020-11-03 17:32:21 +0200

    added route in link

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,12 +3,17 @@ import Navbar from './Navbar/Navbar'
 import Profile from './Profile/Profile'
 import Sidebar from './Sidebar/Sidebar'
 import s from "./main.module.css"
+import {Route} from 'react-router-dom'
+import Dialogs from './Dialogs/Dialogs'
 
 const Main = () => {
     return (
         <div className={s.main_page}>
             <Navbar/>
-            <Profile/>
+            <div className={s.content}>
+                <Route path='/profile' component={Profile} />
+                <Route path='/dialog' component={Dialogs} />
+            </div>
             <Sidebar/>
         </div>
     )
