552812ed5fa4b48b06da25f808fd94aad6caa014
Dima <katri4dimon@i.ua>
2020-11-04 17:41:29 +0200

    added component News, Music, Settings and Route theirs in Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,9 @@ import Sidebar from './Sidebar/Sidebar'
 import s from "./main.module.css"
 import {Route} from 'react-router-dom'
 import Dialogs from './Dialogs/Dialogs'
+import News from './News/News'
+import Music from './Music/Music'
+import Settings from './Settings/Settings'
 
 const Main = () => {
     return (
@@ -13,6 +16,9 @@ const Main = () => {
             <div className={s.content}>
                 <Route path='/profile' component={Profile} />
                 <Route path='/dialog' component={Dialogs} />
+                <Route path='/news' component={News} />
+                <Route path='/music' component={Music} />
+                <Route path='/settings' component={Settings} />
             </div>
             <Sidebar/>
         </div>
