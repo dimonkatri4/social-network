76b7bb9e2bb49efc88dd2ec00f81b6e842811650
Dima <katri4dimon@i.ua>
2020-11-05 17:44:06 +0200

    added dialogs in component Dialogs

--- src/components/Main/Dialogs/Dialogs.jsx
+++ src/components/Main/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import s from './dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={`${s.dialogs} profile_block`}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>Dima</div>
+                <div className={s.dialog}>Petro</div>
+                <div className={s.dialog}>Andriy</div>
+                <div className={s.dialog}>Yuriy</div>
+                <div className={s.dialog}>Oleg</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Do you like coffe?</div>
+            </div>
         </div>
     )
 }
