ca2e70169fbfa2878c87d0279793c0b69717ee8a
Dima <katri4dimon@i.ua>
2020-11-09 20:27:59 +0200

    moved postsDate in level up - in component Profile

--- src/components/Main/Profile/MyPosts/MyPosts.jsx
+++ src/components/Main/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Post from '../Post/Post'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let postsData = [
-        {id:1, message:'Hi, how are you?', likeCount: 10},
-        {id:2, message:'It\'s my first post', likeCount: 5},
-        {id:3, message:'Helo!', likeCount: 7}
-    ]
 
-    let postElement = postsData
+
+    let postElement = props.postsData
         .map( post => <Post message={post.message} likeCount={post.likeCount} />)
 
     return (
--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -4,10 +4,15 @@ import MyPosts from './MyPosts/MyPosts'
 import s from "./profile.module.css"
 
 const Profile = () => {
+    let postsData = [
+        {id:1, message:'Hi, how are you?', likeCount: 10},
+        {id:2, message:'It\'s my first post', likeCount: 5},
+        {id:3, message:'Helo!', likeCount: 7}
+    ]
     return (
         <div className={s.content}>
             <CreatePost/>
-            <MyPosts/>
+            <MyPosts postsData={postsData} />
         </div>
     )
 }
