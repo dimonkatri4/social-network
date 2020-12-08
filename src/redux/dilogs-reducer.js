55edb1109f754bdde608c6752a52d1cbaff8b3b4
Dima <katri4dimon@i.ua>
2020-12-08 19:16:36 +0200

    замінили умову else if на switch-case в dialogs-reducer

--- src/redux/dilogs-reducer.js
+++ src/redux/dilogs-reducer.js
@@ -2,18 +2,21 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 const dialogsReducer = (state, action) => {
-    if (action.type === ADD_MESSAGE) {
-        let newMessage = {
-            id: 6,
-            message: state.newMessageText
-        }
-        state.messages.push(newMessage);
-        state.newMessageText = '';
-    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageText = action.newText;
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let newMessage = {
+                id: 6,
+                message: state.newMessageText
+            }
+            state.messages.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newText;
+            return state;
+        default:
+            return state;
     }
-
-    return state
 }
 
 export const addMessageActionCreator = () => ({ type: ADD_MESSAGE });
