03d3888c9695bfbd4931a449d3b2ecc8e3f99f21
Dima <katri4dimon@i.ua>
2020-12-17 22:25:31 +0200

    рефакторінг profile-reducer i dialogs-reducer

--- src/redux/dilogs-reducer.js
+++ src/redux/dilogs-reducer.js
@@ -3,47 +3,46 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 let initialState = {
     dialogs: [
-        { name: 'Dima', id: '1', img: 'images/users/1.jpg' },
-        { name: 'Oleg', id: '2', img: 'images/users/2.jpg' },
-        { name: 'Yuriy', id: '3', img: 'images/users/3.png' },
-        { name: 'Petro', id: '4', img: 'images/users/4.jpg' },
-        { name: 'Andriy', id: '5', img: 'images/users/5.jpg' }
+        {name: 'Dima', id: '1', img: 'images/users/1.jpg'},
+        {name: 'Oleg', id: '2', img: 'images/users/2.jpg'},
+        {name: 'Yuriy', id: '3', img: 'images/users/3.png'},
+        {name: 'Petro', id: '4', img: 'images/users/4.jpg'},
+        {name: 'Andriy', id: '5', img: 'images/users/5.jpg'}
     ],
     messages: [
-        { id: '1', message: 'Hi!' },
-        { id: '2', message: 'How are you?' },
-        { id: '3', message: 'Do you like coffe?' },
-        { id: '4', message: 'YO!' },
-        { id: '5', message: 'Blablabla' }
+        {id: '1', message: 'Hi!'},
+        {id: '2', message: 'How are you?'},
+        {id: '3', message: 'Do you like coffe?'},
+        {id: '4', message: 'YO!'},
+        {id: '5', message: 'Blablabla'}
     ],
     newMessageText: ''
 }
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_MESSAGE: {
+        case ADD_MESSAGE:
             let newMessage = {
                 id: 6,
                 message: state.newMessageText
             }
-            let stateCopy = {...state};
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = '';
-            return stateCopy;
-        }
-        case UPDATE_NEW_MESSAGE_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newMessageText = action.newText;
-            return stateCopy;
-        }
+            return {
+                ...state,
+                messages: [...state.messages, newMessage],
+                newMessageText: ''
+            };
+        case UPDATE_NEW_MESSAGE_TEXT:
+            return {
+                ...state,
+                newMessageText: action.newText
+            };
         default:
             return state;
     }
 }
 
-export const addMessageActionCreator = () => ({ type: ADD_MESSAGE });
+export const addMessageActionCreator = () => ({type: ADD_MESSAGE});
 export const updateNewMessageTextActionCreator = (text) =>
-    ({ type: UPDATE_NEW_MESSAGE_TEXT, newText: text });
+    ({type: UPDATE_NEW_MESSAGE_TEXT, newText: text});
 
 export default dialogsReducer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,17 +3,53 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
     posts: [
-        { id: 1, message: 'Hi, how are you?', likeCount: 10, userName: 'cat griffith', commentsCount: 5, viewsCount: 9, shareCount: 1, date: 'June,2 2018 19:20' },
-        { id: 2, message: 'It\'s my first post', likeCount: 5, userName: 'cat griffith', commentsCount: 3, viewsCount: 7, shareCount: 2, date: 'June,2 2018 19:20' },
-        { id: 3, message: 'Helo!', likeCount: 7, userName: 'cat griffith', commentsCount: 1, viewsCount: 21, shareCount: 0, date: 'June,2 2018 19:20' },
-        { id: 4, message: 'Hi, how are you?', likeCount: 14, userName: 'cat griffith', commentsCount: 15, viewsCount: 20, shareCount: 3, date: 'June,2 2018 19:20' },
+        {
+            id: 1,
+            message: 'Hi, how are you?',
+            likeCount: 10,
+            userName: 'cat griffith',
+            commentsCount: 5,
+            viewsCount: 9,
+            shareCount: 1,
+            date: 'June,2 2018 19:20'
+        },
+        {
+            id: 2,
+            message: 'It\'s my first post',
+            likeCount: 5,
+            userName: 'cat griffith',
+            commentsCount: 3,
+            viewsCount: 7,
+            shareCount: 2,
+            date: 'June,2 2018 19:20'
+        },
+        {
+            id: 3,
+            message: 'Helo!',
+            likeCount: 7,
+            userName: 'cat griffith',
+            commentsCount: 1,
+            viewsCount: 21,
+            shareCount: 0,
+            date: 'June,2 2018 19:20'
+        },
+        {
+            id: 4,
+            message: 'Hi, how are you?',
+            likeCount: 14,
+            userName: 'cat griffith',
+            commentsCount: 15,
+            viewsCount: 20,
+            shareCount: 3,
+            date: 'June,2 2018 19:20'
+        },
     ],
     newPostText: ''
 }
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST: {
+        case ADD_POST:
             let newPost = {
                 id: 5,
                 message: state.newPostText,
@@ -24,24 +60,24 @@ const profileReducer = (state = initialState, action) => {
                 shareCount: 0,
                 date: 'November, 20 2020 16:43'
             };
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.unshift(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
-        }
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        }
+            return {
+                ...state,
+                posts: [newPost, ...state.posts],
+                newPostText: ''
+            }
+
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
+            }
         default:
             return state
     }
 }
 
-export const addPostActionCreator = () => ({ type: ADD_POST });
+export const addPostActionCreator = () => ({type: ADD_POST});
 export const updateNewPostTextActionCreator = (text) =>
-    ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+    ({type: UPDATE_NEW_POST_TEXT, newText: text});
 
-export default  profileReducer;
+export default profileReducer;
