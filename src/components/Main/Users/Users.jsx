c447120cb61679c0e7719e18c4988934110944d9
Dima <katri4dimon@i.ua>
2020-12-27 21:00:47 +0200

    переробили компоненту Users з функціональної в класову

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import s from "./users.module.css"
 import axios from "axios";
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response=> {
-            props.setUsers(response.data.items)
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
         })
 
         // props.setUsers(
@@ -37,24 +39,30 @@ const Users = (props) => {
         //     ]
         // )
     }
-    return (
-        <div>
-            {
-                props.users.map(u => <div>
-                    <div><img src={u.photos.small ==! null ? u.photos.small : 'https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697' } alt="" className={s.ava}/></div>
-                    <div>{u.followed ?
-                        <button onClick={() => props.unfollow(u.id)}>Follow</button> :
-                        <button onClick={() => props.follow(u.id)}>Unfollow</button>}</div>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                    <div>
-                        <div>"u.location.city"</div>
-                        <div>"u.location.country"</div>
-                    </div>
-                </div>)
-            }
-        </div>
-    )
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div>
+                        <div><img
+                            src={u.photos.small == !null ? u.photos.small : 'https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697'}
+                            alt="" className={s.ava}/></div>
+                        <div>{u.followed ?
+                            <button onClick={() => this.props.unfollow(u.id)}>Follow</button> :
+                            <button onClick={() => this.props.follow(u.id)}>Unfollow</button>}</div>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                        <div>
+                            <div>"u.location.city"</div>
+                            <div>"u.location.country"</div>
+                        </div>
+                    </div>)
+                }
+            </div>
+        )
+    }
 }
+
 
 export default Users
