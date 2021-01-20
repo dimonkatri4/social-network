e02283f56e24ea8e79f90001460b2e2e150f1dd2
Dima <katri4dimon@i.ua>
2021-01-20 17:39:09 +0200

    створили auth-reducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dilogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebarPage: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 })
 
 let store = createStore(reducers);
