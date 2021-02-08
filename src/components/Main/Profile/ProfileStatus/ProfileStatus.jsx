4157ee31e9e57d24c29c5b952428c6d99b2040ad
Dima <katri4dimon@i.ua>
2021-02-08 22:04:13 +0200

    added comment

--- src/components/Main/Profile/ProfileStatus/ProfileStatus.jsx
+++ src/components/Main/Profile/ProfileStatus/ProfileStatus.jsx
@@ -28,7 +28,7 @@ class ProfileStatus extends React.Component {
 
     componentDidUpdate(prevProps,prevState) {
         debugger
-        if(this.props.status !== prevProps.status) {
+        if(this.props.status !== prevProps.status) {                    // умова щоб уникнути зациклення
             this.setState({
                 status: this.props.status
             })
