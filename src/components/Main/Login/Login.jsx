710e7fe4788fdc7740f89debcf93c02be3ffe735
Dima <katri4dimon@i.ua>
2021-02-09 22:05:00 +0200

    використали redux-form для сторінки Login

--- src/components/Main/Login/Login.jsx
+++ src/components/Main/Login/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field placeholder={'Login'} component={'input'} name={'login'}/>
+        </div>
+        <div>
+            <Field placeholder={'Password'} component={'input'} name={'password'}/>
+        </div>
+        <div>
+            <Field type={'checkbox'} name={'rememberMe'} component={'input'}/>Remember Me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = (props) => {
-    return <h1>Login</h1>
+   const onSubmit = (submit) => {
+        console.log(submit)
+    }
+    return <>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </>
+
 }
+
 
 export default Login
