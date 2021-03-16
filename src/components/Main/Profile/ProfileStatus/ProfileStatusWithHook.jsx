2c1f097427b8b4e137c1b68c4f73f65e48e819ce
Dima <katri4dimon@i.ua>
2021-03-16 22:46:36 +0200

    використали хук useEffect для того щоб виконати сайд-ефект після того як компонента відрендерилась

--- src/components/Main/Profile/ProfileStatus/ProfileStatusWithHook.jsx
+++ src/components/Main/Profile/ProfileStatus/ProfileStatusWithHook.jsx
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import style from "./profileStatus.module.css"
 
 const ProfileStatusWithHook = (props) =>  {
 
     let [editMode, setEditMode] = useState(false);
-    let [status, setStatus] = useState(props.status)
+    let [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status)
+    }, [props.status])
 
    const activateEditMode = () => {
         setEditMode(true)
