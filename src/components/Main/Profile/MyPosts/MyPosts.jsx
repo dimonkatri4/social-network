ceedce694c6626cbb06230fd019b9bb62889be0a
Dima <katri4dimon@i.ua>
2021-03-29 21:38:22 +0300

    змінили компонент MyPost з класової на функціональну і використали HOC React.memo замість PureComponent в класовій компоненті

--- src/components/Main/Profile/MyPosts/MyPosts.jsx
+++ src/components/Main/Profile/MyPosts/MyPosts.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
 import Post from '../Post/Post'
 
-class MyPosts extends React.PureComponent {
+const MyPosts = React.memo((props) => {
+    console.log("MYPOST");
 
-/*    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps !== this.props || nextState !== this.state
-    }*/
+    let postElement = props.posts
+        .map(post => <Post
+            message={post.message}
+            likeCount={post.likeCount}
+            userName={post.userName}
+            commentsCount={post.commentsCount}
+            viewsCount={post.viewsCount}
+            shareCount={post.shareCount}
+            date={post.date}
+            key={post.id}
+        />)
 
-    render() {
-
-        console.log("MYPOST")
-
-        let postElement = this.props.posts
-            .map(post => <Post
-                message={post.message}
-                likeCount={post.likeCount}
-                userName={post.userName}
-                commentsCount={post.commentsCount}
-                viewsCount={post.viewsCount}
-                shareCount={post.shareCount}
-                date={post.date}
-                key={post.id}
-            />)
-
-        return (
-            <div>
-                {postElement}
-            </div>
-        )
-    }
-}
+    return (
+        <div>
+            {postElement}
+        </div>
+    )
+});
 
 export default MyPosts
