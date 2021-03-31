36e6ba9fba70220cec59ce826a08ac8237fc7d0c
Dima <katri4dimon@i.ua>
2021-03-31 19:42:01 +0300

    винесли розмітку для відображення одного користувача в компоненту User

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import style from "./users.module.css";
-import {NavLink} from "react-router-dom";
 import Pagination from "../../common/Pagination/Pagination";
+import User from "./User";
 
-let Users = ({totalUsersCount,pageSize,currentPage,onPageChanged,follow,unfollow,...props}) => {
+let Users = ({totalUsersCount, pageSize, currentPage, onPageChanged, follow, unfollow, ...props}) => {
     return (
         <div className="profile_block">
             <Pagination
@@ -12,28 +11,16 @@ let Users = ({totalUsersCount,pageSize,currentPage,onPageChanged,follow,unfollow
                 pageSize={pageSize}
                 onPageChanged={onPageChanged}
             />
-            {
-                props.users.map(u => <div>
-                    <div><NavLink to={'/profile/' + u.id}> <img
-                        src={u.photos.small !== null ? u.photos.small : 'https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697'}
-                        alt="" className={style.ava}/></NavLink></div>
-                    <div>{u.followed ?
-                        <button disabled={props.followingInProgress.some(id => id === u.id )} onClick={() => {
-                            unfollow(u.id)
-                        }
-                        }>Unfollow</button> :
-                        <button disabled={props.followingInProgress.some(id => id === u.id )} onClick={() => {
-                            follow(u.id)
-                        }
-                        }>Follow</button>}</div>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                    <div>
-                        <div>"u.location.city"</div>
-                        <div>"u.location.country"</div>
-                    </div>
-                </div>)
-            }
+            <div>
+                {
+                    props.users.map(u => <User
+                        user={u}
+                        followingInProgress={props.followingInProgress}
+                        follow={follow}
+                        unfollow={unfollow}
+                    />)
+                }
+            </div>
         </div>
     )
 }
