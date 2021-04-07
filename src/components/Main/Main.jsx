8aa9992e9d25e879424a37b0ee7397fa60a2828e
Dima <katri4dimon@i.ua>
2021-04-07 22:33:35 +0300

    використали ф-ї React.lazy для лінивої загрузки компоненти - загружається тільки тоді коли користувач переходить на неї, і огортаєм компоненту яка буде ліниво загружатись компонентою React.Suspense - для того щоб показати користувачу що даны грузяться - fallback

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import Navbar from './Navbar/Navbar'
-import Sidebar from './Sidebar/Sidebar'
-import s from "./main.module.css"
-import { Route } from 'react-router-dom'
-import Dialogs from './Dialogs/Dialogs'
-import News from './News/News'
-import Music from './Music/Music'
-import Settings from './Settings/Settings'
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+import Sidebar from './Sidebar/Sidebar';
+import s from './main.module.css';
+import {Route} from 'react-router-dom';
+//import Dialogs from './Dialogs/Dialogs';
+import News from './News/News';
+import Music from './Music/Music';
+import Settings from './Settings/Settings';
 import UsersContainer from "./Users/UsersContainer";
-import ProfileContainer from "./Profile/ProfileContainer";
+//import ProfileContainer from "./Profile/ProfileContainer";
 import Login from "./Login/Login";
+const Dialogs = React.lazy(() => import('./Dialogs/Dialogs'));
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'));
 
 const Main = (props) => {
 
     return (
         <div className={s.main_page}>
-            <Navbar />
-
-                <Route path='/profile/:userId?' render={() => <ProfileContainer
-                    profilePage={props.profilePage}
-                    />}
-                />
+            <Navbar/>
+            <Route path='/profile/:userId?' render={() =>
+                <React.Suspense fallback={<div>Loading..</div>}>
+                    <ProfileContainer profilePage={props.profilePage}/>
+                </React.Suspense>
+            }
+            />
             <div className={s.content}>
                 <Route path='/dialog' render={() =>
-                    <Dialogs
-                    dialogsPage = {props.dialogsPage}
-                    isAuth={props.isAuth}
-                    />}
-                />
-                <Route path='/news' component={News} />
-                <Route path='/music' component={Music} />
-                <Route path='/settings' component={Settings} />
-                <Route path='/users' component={UsersContainer} />
+                    <React.Suspense fallback={<div>Loading..</div>}>
+                        <Dialogs dialogsPage={props.dialogsPage} isAuth={props.isAuth}/>
+                    </React.Suspense>
+                }/>
+                <Route path='/news' component={News}/>
+                <Route path='/music' component={Music}/>
+                <Route path='/settings' component={Settings}/>
+                <Route path='/users' component={UsersContainer}/>
                 <Route path='/login' component={Login}/>
             </div>
-            <Sidebar sidebarPage={props.sidebarPage} />
+            <Sidebar sidebarPage={props.sidebarPage}/>
         </div>
     )
 }
