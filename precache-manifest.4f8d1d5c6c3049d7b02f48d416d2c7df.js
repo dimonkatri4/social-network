e45c9a2cc14488d1c6e69bc6c7ca1ff22e7e720c
Dima <katri4dimon@i.ua>
2021-04-11 14:24:27 +0300

    Updates

---
+++ precache-manifest.4f8d1d5c6c3049d7b02f48d416d2c7df.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c24371535ba2e36034dbbf751cd909e",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "b91dfdd44e26fb553823",
+    "url": "/social-network/static/css/3.d1a18af1.chunk.css"
+  },
+  {
+    "revision": "5a0e8aba94eb4486847d",
+    "url": "/social-network/static/css/4.4c6aa6ff.chunk.css"
+  },
+  {
+    "revision": "1d2d773861641e04891d",
+    "url": "/social-network/static/css/main.d361f8f3.chunk.css"
+  },
+  {
+    "revision": "5c565f6eabf5eb7a5c07",
+    "url": "/social-network/static/js/2.2e66cad1.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/social-network/static/js/2.2e66cad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91dfdd44e26fb553823",
+    "url": "/social-network/static/js/3.160c4f91.chunk.js"
+  },
+  {
+    "revision": "5a0e8aba94eb4486847d",
+    "url": "/social-network/static/js/4.11a88184.chunk.js"
+  },
+  {
+    "revision": "1d2d773861641e04891d",
+    "url": "/social-network/static/js/main.91bf0fb4.chunk.js"
+  },
+  {
+    "revision": "d524cced6ec1cdc4fa3e",
+    "url": "/social-network/static/js/runtime-main.612063e3.js"
+  },
+  {
+    "revision": "b9c1b183325b35f23a2c883d96f18679",
+    "url": "/social-network/static/media/1.b9c1b183.jpg"
+  },
+  {
+    "revision": "55c1dbf6f610e4903478d54165af9ea9",
+    "url": "/social-network/static/media/2.55c1dbf6.jpg"
+  },
+  {
+    "revision": "c72cb17ea588165f8a38ec9956732adb",
+    "url": "/social-network/static/media/3.c72cb17e.png"
+  },
+  {
+    "revision": "43fe2e8402b1ba6572f38d136a2797d2",
+    "url": "/social-network/static/media/4.43fe2e84.jpg"
+  },
+  {
+    "revision": "bf70c3db7c9d77c4bb8fd400f7e763d9",
+    "url": "/social-network/static/media/5.bf70c3db.jpg"
+  },
+  {
+    "revision": "3550749a7aba422388c6aa8df4347284",
+    "url": "/social-network/static/media/bell.3550749a.png"
+  },
+  {
+    "revision": "a5eadb8d2ce65df8340ccefe1032513c",
+    "url": "/social-network/static/media/comments.a5eadb8d.png"
+  },
+  {
+    "revision": "e2d7a28f0222371d4a75db520d62c7be",
+    "url": "/social-network/static/media/cover-photo.e2d7a28f.jpg"
+  },
+  {
+    "revision": "bb4f977b4efa2428236f405d3881b49a",
+    "url": "/social-network/static/media/eye.bb4f977b.png"
+  },
+  {
+    "revision": "c0113d5adf41065aa539edda4e51815f",
+    "url": "/social-network/static/media/home.c0113d5a.png"
+  },
+  {
+    "revision": "5621d21557d9aeff89b3617b6c2f5c31",
+    "url": "/social-network/static/media/loupe.5621d215.png"
+  },
+  {
+    "revision": "98bce3060b56b8816c7313f7da3640af",
+    "url": "/social-network/static/media/love.98bce306.png"
+  },
+  {
+    "revision": "4c9a51c96bf634be40400d15c5791e53",
+    "url": "/social-network/static/media/music.4c9a51c9.png"
+  },
+  {
+    "revision": "ef09e6b9b006b3e1c784643781d752b1",
+    "url": "/social-network/static/media/photo-camera.ef09e6b9.png"
+  },
+  {
+    "revision": "66654ff38839c57de0925c2ecbf28b1f",
+    "url": "/social-network/static/media/photo-user.66654ff3.jpg"
+  },
+  {
+    "revision": "97d3466c78b4e74c155e0f1bf60750d3",
+    "url": "/social-network/static/media/picture.97d3466c.png"
+  },
+  {
+    "revision": "f87b2d173e6a3d1f186c7acd8e789122",
+    "url": "/social-network/static/media/search.f87b2d17.png"
+  },
+  {
+    "revision": "997bfd240a471c077bbb23b3090a8662",
+    "url": "/social-network/static/media/share.997bfd24.png"
+  },
+  {
+    "revision": "bb381c7efd40393a12a20bf6e6826f92",
+    "url": "/social-network/static/media/video-camera.bb381c7e.png"
+  },
+  {
+    "revision": "62b6186425be4117031ff4c2d3f7a2fa",
+    "url": "/social-network/static/media/world.62b61864.png"
+  }
+]);
