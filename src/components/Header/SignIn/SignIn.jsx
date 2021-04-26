5d6f573457fe342501c3499e518a08c127889187
Dima <katri4dimon@i.ua>
2021-04-26 22:59:35 +0300

    realization SignIn

--- src/components/Header/SignIn/SignIn.jsx
+++ src/components/Header/SignIn/SignIn.jsx
@@ -1,18 +1,47 @@
 import React from "react";
 import style from './signIn.module.css'
+import {Field, reduxForm} from "redux-form";
+import {Input} from "../../common/FormsControl/FormsControl";
+import {email, required} from "../../../utils/validators/validators";
+import {Redirect} from "react-router-dom";
+import {login} from "../../../redux/auth-reducer";
+import {connect} from "react-redux";
 
-const SignIn = (props) => {
+
+const SignInForm = (props) => {
     return <div>
-        <div className={style.inputPlace}>
+        <form onSubmit={props.handleSubmit}>
+            <div className={style.inputPlace}>
+                <Field placeholder={'User Name'} component={Input} name={'login'} validate={[required,email]} />
+                <Field placeholder={'Password'} component={Input} name={'password'} validate={[required]}
+                       type={'password'} />
+                { props.error && <div className={style.commonErrorLogin}>{props.error}</div>}
+                <button className="button">Log In</button>
+            </div>
+        </form>
+{/*        <div className={style.inputPlace}>
             <input type="text" placeholder="User Name"/>
             <input type="password" placeholder="Password"/>
             <button className="button">Log In</button>
-        </div>
-        <div>
-
-        </div>
-
+        </div>*/}
     </div>
 }
 
-export default SignIn
+const SignInReduxForm = reduxForm({form:"login"})(SignInForm)
+
+const SignIn = (props) => {
+    const onSubmit = (formData) => {
+        props.login(formData.login, formData.password)
+    }
+    if(props.isAuth) {
+        return <Redirect to='/profile' />
+    }
+    return <>
+        <SignInReduxForm onSubmit={onSubmit}/>
+    </>
+}
+
+let mapStateToProps = (state) => ({
+    isAuth:state.auth.isAuth
+})
+export default connect(mapStateToProps,{login})(SignIn)
