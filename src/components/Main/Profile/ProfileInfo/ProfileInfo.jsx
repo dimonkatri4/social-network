4f8e666cbbed0137472e021372615b887b2dbb54
Dima <katri4dimon@i.ua>
2021-05-05 23:58:22 +0300

    added new component ProfileInfo

---
+++ src/components/Main/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./profileInfo.module.css"
+import classNames from "classnames";
+
+const ProfileInfo = () => {
+    return <div className={classNames(style.profileInfo, 'profile_block')}>
+        Hello
+    </div>
+
+}
+
+export default ProfileInfo
