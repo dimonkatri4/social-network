cb954377cb5044aff77630bae3de80c87f30b2b0
Dima <katri4dimon@i.ua>
2021-05-07 16:54:25 +0300

    added Contacts in ProfileInfo

--- src/components/Main/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Main/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./profileInfo.module.css"
 import classNames from "classnames";
 import Preloader from "../../../common/Preloader/Preloader";
+import {faInstagram} from '@fortawesome/free-solid-svg-icons'
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
@@ -17,9 +18,17 @@ const ProfileInfo = (props) => {
         <div>
             Looking for a job: {props.profile.lookingForAJob ? "Yes" : "No" }
         </div>
-        {props.profile.lookingForAJob && <div>Description job: {props.profile.lookingForAJobDescription}</div>}
+        {props.profile.lookingForAJobDescription && <div>Description job: {props.profile.lookingForAJobDescription}</div>}
+        {Object.keys(props.profile.contacts).map((key)=> {
+            return <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key]}  />
+        })}
     </div>
+}
 
+const Contacts = ({contactTitle, contactValue}) => {
+    return <div>
+        {contactValue && <div><b>{contactTitle}</b>: {contactValue}</div>}
+    </div>
 }
 
 export default ProfileInfo
