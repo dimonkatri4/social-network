6ea573a71d544c5cec5733ddf108b2d60d4bf0a9
Dima <katri4dimon@i.ua>
2021-06-30 14:48:43 +0300

    update some functional in ProfileStatus

--- src/components/Main/Profile/ProfileStatus/ProfileStatusWithHook.jsx
+++ src/components/Main/Profile/ProfileStatus/ProfileStatusWithHook.jsx
@@ -1,29 +1,52 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import style from "./profileStatus.module.css"
 import classNames from "classnames";
-import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {faTimes, faCheck} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const ProfileStatusWithHook = (props) =>  {
+const ProfileStatusWithHook = (props) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
 
-    useEffect( () => {
+    useEffect(() => {
         setStatus(props.status);
         props.setErrorInStatus(null)
-    }, [props.status])
+    }, [props.status,editMode]);
 
-   const activateEditMode = () => {
+    const useOutsideClick = (ref) => {
+        useEffect(() => {
+            // Edit state if clicked on outside of element
+            const handleClickOutside = e => {
+                if (editMode && !ref.current.contains(e.target)) {
+                    exitOfInputStatus();
+                }
+            }
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                // Unbind the event listener on clean up
+                document.removeEventListener("mousedown", handleClickOutside);
+            }
+        }, [ref,editMode]);
+    }
+    const wrapperRef = useRef(null);
+    useOutsideClick(wrapperRef);
+
+    const activateEditMode = () => {
         setEditMode(true)
     }
-    const deactivateEditMode = () => {
+    const updateProfileStatus = () => {
         props.updateStatus(status)
-        if (status.length <= 300){
-        setEditMode(false)
+        if (status.length <= 300) {
+            setEditMode(false)
         } else {
             props.setErrorInStatus()
         }
+    }
+    const exitOfInputStatus = () => {
+        setEditMode(false);
+        setStatus(props.status);
     }
 
     const onStatusChange = (e) => {
@@ -31,23 +54,30 @@ const ProfileStatusWithHook = (props) =>  {
         setStatus(text);
     }
 
-        return <div className={style.statusBlock}>
-            {!editMode &&
-            <span className={style.textStatus} onDoubleClick={props.isOwner && activateEditMode} title="Double click to change status">{props.status
-            || props.isOwner && <span className={style.addStatus}>Double click to add status</span>} </span>
-            }
-            {editMode &&
-                <div className={style.inputBlock}>
-                <input className={classNames("inputPlace",style.inputStatus)} onChange={onStatusChange}
-                       onBlur={deactivateEditMode}  autoFocus={true}  value={status} />
-                       <span className={style.close}><FontAwesomeIcon icon={faTimes} onClick={() => setStatus(null)}/></span>
-                </div>
-            }
-            {
-                props.errorInStatus && <div className={style.error}>{props.errorInStatus}</div>
-            }
+    const handleKeyDown = (e) => {
+        e.key === 'Enter' && updateProfileStatus();
+        e.key === 'Escape' && exitOfInputStatus();
+    }
 
+    return <div className={style.statusBlock}>
+        {!editMode &&
+        <span className={style.textStatus} onDoubleClick={props.isOwner && activateEditMode}
+              title="Double click to change status">{props.status
+        || props.isOwner && <span className={style.addStatus}>Double click to add status</span>} </span>
+        }
+        {editMode &&
+        <div className={style.inputBlock} ref={wrapperRef}>
+            <input className={classNames("inputPlace", style.inputStatus)} onChange={onStatusChange}
+                   autoFocus={true} value={status} onKeyDown={handleKeyDown}/>
+            <span className={style.updateStatus}><FontAwesomeIcon icon={faCheck} onClick={() => updateProfileStatus()}/></span>
+            <span className={style.close}><FontAwesomeIcon icon={faTimes} onClick={exitOfInputStatus}/></span>
         </div>
+        }
+        {
+            props.errorInStatus && <div className={style.error}>{props.errorInStatus}</div>
+        }
+
+    </div>
 
 }
 export default ProfileStatusWithHook
