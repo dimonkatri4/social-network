8cbe37ee923241242666ea5a4a58a2fdb4d71baa
Dima <katri4dimon@i.ua>
2021-07-01 08:27:23 +0300

    some change in CreatePost

--- src/components/Main/Profile/CreatePost/CreatePost.jsx
+++ src/components/Main/Profile/CreatePost/CreatePost.jsx
@@ -12,36 +12,37 @@ import {Textarea} from "../../../common/FormsControl/FormsControl";
 const maxLength20 = maxLength(20)
 
 const PostForm = (props) => {
-    return  <form onSubmit={props.handleSubmit}  >
-        <Field
-            component={Textarea}
-            name={'messageText'}
-            rows="8"
-            placeholder="write something"
-            validate = {[required, maxLength20]}
-        />
-
-        <div className={s.panel}>
-            <a href="/"><img src={musicIcon} alt="music" /></a>
-            <a href="/"><img src={pictureIcon} alt="picture_icon" /></a>
-            <a href="/"><img src={videoIcon} alt="video-camera" /></a>
-            <a href="/"><img src={photoIcon} alt="photo_camera" /></a>
-            <button className='button'>Publish</button>
+    return <form onSubmit={props.handleSubmit}>
+        <div className={s.inputText}>
+            <Field
+                component={Textarea}
+                name={'messageText'}
+                rows="8"
+                placeholder="write something"
+                validate={[required, maxLength20]}
+            />
+            <div className={s.panel}>
+                <a href="/"><img src={musicIcon} alt="music"/></a>
+                <a href="/"><img src={pictureIcon} alt="picture_icon"/></a>
+                <a href="/"><img src={videoIcon} alt="video-camera"/></a>
+                <a href="/"><img src={photoIcon} alt="photo_camera"/></a>
+                <button className='button'>Publish</button>
+            </div>
         </div>
     </form>
 }
 
-const PostFormRedux = reduxForm({form:'createPost'})(PostForm)
+const PostFormRedux = reduxForm({form: 'createPost'})(PostForm)
 
 const CreatePost = (props) => {
     const onSubmit = (formData) => {
         props.addPost(formData.messageText)
     }
     return (
-        <div className={`${s.profile_block} profile_block`} >
+        <div className={`${s.profile_block} profile_block`}>
             <div className={s.create_post}>
-                <img className={s.ava} src={photoUser} alt="avatar" />
-                <PostFormRedux onSubmit={onSubmit} />
+                <img className={s.ava} src={photoUser} alt="avatar"/>
+                <PostFormRedux onSubmit={onSubmit}/>
             </div>
         </div>
     )
