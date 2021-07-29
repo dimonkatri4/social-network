db9b98a1b103a089a7d9a55503e184c8582e1e27
Dima <katri4dimon@i.ua>
2021-07-29 22:50:18 +0300

    added some style in modal window profile main photo

--- src/components/Main/ModalWindow/ModalWindow.jsx
+++ src/components/Main/ModalWindow/ModalWindow.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import style from "./modalWindow.module.css"
 
 const ModalWindow = ({active, setActive,children}) => {
-    return <div className={active ? `${style.modal} ${style.active}` : `${style.modal}`}
+    return <div className={active ? `${style.modal} ${style.modal_active}` : `${style.modal}`}
                 onClick={() => setActive(false)}>
-        <div className={style.modal_content}
-             onClick={(e) => e.stopPropagation()}></div>
-        {children}
+        <div className={active ? `${style.modal_content} ${style.content_active}` : `${style.modal_content}`}
+             onClick={(e) => e.stopPropagation()}>
+            {children}
+        </div>
+
     </div>
 }
 
