888ea0f04692077fcef3b0ad08e623910c3d3d2f
Dima <katri4dimon@i.ua>
2021-10-22 14:18:22 +0300

    Migration from Redux Form in CreatePost component

--- src/components/Main/Profile/CreatePost/CreatePost.jsx
+++ src/components/Main/Profile/CreatePost/CreatePost.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import s from "./createPost.module.css"
-import {Field, reduxForm} from "redux-form";
+import {Field, Form} from "react-final-form";
 import {maxLength, required} from "../../../../utils/validators/validators";
 import {Textarea} from "../../../common/FormsControl/FormsControl";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -9,36 +9,42 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const maxLength20 = maxLength(20)
 
 const PostForm = (props) => {
-    return <form onSubmit={props.handleSubmit}>
-        <div className={s.inputText}>
-            <Field
-                component={Textarea}
-                name={'messageText'}
-                placeholder="write something"
-            />
-            <div className={s.panel}>
-                <span className={s.iconPanel}><FontAwesomeIcon icon="music"/></span>
-                <span className={s.iconPanel}><FontAwesomeIcon icon="image"/></span>
-                <span className={s.iconPanel}><FontAwesomeIcon icon="video"/></span>
-                <span className={s.iconPanel}><FontAwesomeIcon icon="camera"/></span>
-                <button className='button'>Publish</button>
-            </div>
-        </div>
-    </form>
-}
+    return (
+        <Form onSubmit={props.onSubmit}>
+            {({handleSubmit,form})=>(
+            <form onSubmit={handleSubmit}>
+                <div className={s.inputText}>
+                    <Field
+                        component={Textarea}
+                        name={'messageText'}
+                        placeholder="write something"
+                    />
+                    <div className={s.panel}>
+                        <span className={s.iconPanel}><FontAwesomeIcon icon="music"/></span>
+                        <span className={s.iconPanel}><FontAwesomeIcon icon="image"/></span>
+                        <span className={s.iconPanel}><FontAwesomeIcon icon="video"/></span>
+                        <span className={s.iconPanel}><FontAwesomeIcon icon="camera"/></span>
+                        <button className="button"  disabled={form.submitting || form.pristine} >Publish</button>
+                    </div>
+                </div>
+            </form>
+            )}
+        </Form>
+    )}
 
-const PostFormRedux = reduxForm({form: 'createPost'})(PostForm)
+//const PostFormRedux = reduxForm({form: 'createPost'})(PostForm)
 
 const CreatePost = (props) => {
 
     const onSubmit = (formData) => {
         props.addPost(formData.messageText);
+        formData.messageText = "";
     }
     return (
         <div className={`${s.profile_block} profile_block`}>
             <div className={s.create_post}>
                 {props.profileOwner && <img className={s.ava} src={props.profileOwner.photos.small} alt="avatar"/>}
-                <PostFormRedux onSubmit={onSubmit}/>
+                <PostForm onSubmit={onSubmit}/>
             </div>
         </div>
     )
